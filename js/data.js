7dce6d65ebbe47f84fd0ac446de33f8a69e5ac0a
ebbaj <ebba-jonsson@hotmail.com>
2021-05-27 23:04:07 +0200

    Upload project

---
+++ js/data.js
@@ -0,0 +1,70 @@
+export const projects = [
+  {
+    id: 1,
+    name: "Rock Paper Scissors",
+    image: {
+      src: "images/image.jpg",
+      alt: " ",
+    },
+    description:
+      "Play against the computer in a simple game of Rock Paper Scissors.",
+    link: "Go to page",
+    url: "https://ebbaj.github.io/rock-paper-scissors/",
+  },
+  {
+    id: 2,
+    name: "The Perfect Night",
+    image: {
+      src: "images/image.jpg",
+      alt: " ",
+    },
+    description: "Find the perfect cocktail or play Pass the Bomb.",
+    link: "Go to page",
+    url: "#",
+  },
+  {
+    id: 3,
+    name: "Movie Watchlist",
+    image: {
+      src: "",
+      alt: " ",
+    },
+    description: "Create you own movie watchlist. Built with React.",
+    link: "Go to page",
+    url: "#",
+  },
+  {
+    id: 4,
+    name: "Pomodoro clock",
+    image: {
+      src: "",
+      alt: " ",
+    },
+    description: "Get productive with this pomodoro clock. Built with React.",
+    link: "Go to page",
+    url: "#",
+  },
+  {
+    id: 5,
+    name: "Ricola Footsteps",
+    image: {
+      src: "",
+      alt: " ",
+    },
+    description:
+      "Student collaboration between Hyper Island FED/UX and Ricola.",
+    link: "Go to page",
+    url: "#",
+  },
+  {
+    id: 5,
+    name: "Settr",
+    image: {
+      src: "",
+      alt: " ",
+    },
+    description: "Hyper Island student project in collaboration with Settr.",
+    link: "Go to page",
+    url: "#",
+  },
+];
---
+++ js/main.js
@@ -0,0 +1,42 @@
+import { projects } from "./data.js";
+
+const projectGrid = document.getElementById("project-grid");
+
+window.onload = () => {
+  projects.forEach((project) => {
+    const box = document.createElement("div");
+    const img = document.createElement("img");
+    const boxContent = document.createElement("div");
+    const boxTitle = document.createElement("h3");
+    const boxParagraph = document.createElement("p");
+    const boxLink = document.createElement("a");
+
+    box.id = project.id;
+
+    box.className = "box";
+    boxContent.className = "box-content";
+    boxTitle.className = "box-title";
+
+    img.src = project.image.src;
+    img.alt = project.image.alt;
+    box.appendChild(img);
+
+    if (project.image.src === "") {
+      img.src = "images/default.jpg";
+      img.alt = "Default image.";
+    }
+
+    boxTitle.innerHTML = project.name;
+    boxContent.appendChild(boxTitle);
+
+    boxParagraph.innerHTML = project.description;
+    boxContent.appendChild(boxParagraph);
+
+    boxLink.innerHTML = project.link;
+    boxLink.href = project.url;
+    boxContent.appendChild(boxLink);
+
+    box.appendChild(boxContent);
+    projectGrid.appendChild(box);
+  });
+};
